c5d8062dc252471cfb53f89fe96afee21f5b790c
Daniel Oaks <daniel@danieloaks.net>
2015-06-29 17:26:15 +1000

    env: Install python-devel, pip

--- environment/shell/2.repos.2.packages.sh
+++ environment/shell/2.repos.2.packages.sh
@@ -21,6 +21,12 @@ yum -y groupinstall 'Development Tools' >/dev/null
 echo 'Finished installing Development Tools'
 # >>> End of PuPHPet Initial Setup
 
+echo 'Installing Python 2 - devel, pip'
+yum -y install python-devel >/dev/null
+yum -y install python-pip >/dev/null
+echo 'Finished installing Python 2 - devel, pip'
+
 echo 'Installing Python 3.4'
 yum -y install python34u >/dev/null
+yum -y install python34u-pip >/dev/null
 echo 'Finished installing Python 3.4'
