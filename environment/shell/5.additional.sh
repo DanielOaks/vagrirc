9b06c5c26f68d3a551d0ab04af7236b9acb40ecd
Daniel Oaks <daniel@danieloaks.net>
2015-06-29 20:08:05 +1000

    env: Install new GCC, simplify some installation

--- environment/shell/5.additional.sh
+++ environment/shell/5.additional.sh
@@ -3,19 +3,41 @@
 
 # Start of Additional Packages Setup
 echo 'Installing additional packages'
-yum -y install python-devel >/dev/null
-yum -y install python-pip >/dev/null
+yum -y install python27 python27-devel python27-tools python27-pip >/dev/null
 
-yum -y install python34u >/dev/null
-yum -y install python34u-pip >/dev/null
+yum -y install python34u python34u-devel python34u-pip >/dev/null
 
 yum -y install mysql-devel >/dev/null
 echo 'Finished installing additional packages'
 # End of Additional Packages Setup
 
+# Start of GCC 4.8 Setup
+echo 'Installing GCC 4.8'
+DEVTOOLS2='http://people.centos.org/tru/devtools-2/devtools-2.repo'
+DEVTOOLS2_CACHE="${CACHE_FOLDER}/$(echo ${DEVTOOLS2} | awk -F/ '{print $NF}')"
+
+mkdir -p "$CACHE_FOLDER"
+if [ ! -e "${DEVTOOLS2_CACHE}" ]; then
+    wget "${DEVTOOLS2}" -O "${DEVTOOLS2_CACHE}"
+fi
+
+cp "${DEVTOOLS2_CACHE}" /etc/yum.repos.d
+
+yum --enablerepo=testing-devtools-2-centos-6 install devtoolset-2-gcc devtoolset-2-gcc-c++
+
+if [[ -f '/root/.bashrc' ]] && ! grep -q 'export PATH="/opt/rh/devtoolset-2/root/usr/bin/:$PATH"' /root/.bashrc; then
+    echo 'export PATH="/opt/rh/devtoolset-2/root/usr/bin/:$PATH"' >> /root/.bashrc
+fi
+if [[ -f '/etc/profile' ]] && ! grep -q 'export PATH="/opt/rh/devtoolset-2/root/usr/bin/:$PATH"' /etc/profile; then
+    echo 'export PATH="/opt/rh/devtoolset-2/root/usr/bin/:$PATH"' >> /etc/profile
+fi
+echo 'Finished installing GCC 4.8'
+# End of GCC 4.8 Setup
+
 # Start of JDK Setup
 echo 'Installing JDK 1.8.0'
 yum -y install java-1.8.0-openjdk >/dev/null
+yum -y install java-1.8.0-openjdk-devel >/dev/null
 
 if [[ -f '/root/.bashrc' ]] && ! grep -q 'export JAVA_HOME="/usr/lib/jvm/jre-1.8.0-openjdk"' /root/.bashrc; then
     echo 'export JAVA_HOME="/usr/lib/jvm/jre-1.8.0-openjdk"' >> /root/.bashrc
